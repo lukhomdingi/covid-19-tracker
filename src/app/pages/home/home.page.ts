25365dfc962d6620695a40b4431855c95cea2df9
Lukho Mdingi <lukho@bbd.co.za>
2020-04-17 19:40:04 +0200

    Implemented the homepage.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '@services/api.service';
+import { StateService } from '@services/state.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  accessToken: string;
+  private allCases: number;
+  private confirmedCases: number;
+  private suspectedCases: number;
+  private deaths: number;
+  private recoveredCases: number;
+  constructor(private apiService: ApiService, private stateService: StateService) {}
+  ngOnInit() {
+    this.accessToken = this.stateService.AccessToken;
+    this.update();
+  }
 
-  constructor() {}
+  get AllCases() {
+    return this.allCases;
+  }
+
+  get ConfirmedCases() {
+    return this.confirmedCases;
+  }
+
+  get SuspectedCases() {
+    return this.suspectedCases;
+  }
+
+  get Deaths() {
+    return this.deaths;
+  }
+
+  get RecoveredCases() {
+    return this.recoveredCases;
+  }
+
+  update() {
+    this.getAllCases();
+    this.getConfirmedCases();
+    this.getSuspectedCases();
+    this.getDeaths();
+    this.getRecoveredCases();
+  }
+
+  async getAllCases() {
+    this.allCases = await this.apiService.getAllCases(this.accessToken);
+  }
+
+  async getConfirmedCases() {
+    this.confirmedCases = await this.apiService.getConfirmedCases(this.accessToken);
+  }
+
+  async getSuspectedCases() {
+    this.suspectedCases = await this.apiService.getSuspectedCases(this.accessToken);
+  }
+
+  async getDeaths() {
+    this.deaths = await this.apiService.getDeaths(this.accessToken);
+  }
+
+  async getRecoveredCases() {
+    this.recoveredCases = await this.apiService.getRecovered(this.accessToken);
+  }
 
 }
