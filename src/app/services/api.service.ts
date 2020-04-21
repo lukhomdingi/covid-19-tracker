d2a44274a320bf09f7814dadf405f4f2ff8b78d9
Lukho Mdingi <lukho@bbd.co.za>
2020-04-21 18:20:56 +0200

    Using the native HTTP plugin.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,81 +1,74 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HTTP, HTTPResponse } from '@ionic-native/http/ngx';
 import { Api, Uri } from '@models/api.model';
-import { AccessTokenResponse, CaseResponse } from '@models/response.model';
-
 
 @Injectable()
 export class ApiService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HTTP) { }
 
   getAccessToken(): Promise<string> {
     return new Promise<string>((resolve, reject) => {
       const url = Uri.ACCESS_TOKEN;
-      const headers = new HttpHeaders();
-      headers.set('Authorization', `Basic ${Api.KEY}`);
-      this.http.post<AccessTokenResponse>(url, {}, {headers}).subscribe((response: AccessTokenResponse) => {
-        resolve(response.access_token);
-      }, (error) => reject(error));
+      const headers = {Authorization: `Basic ${Api.KEY}`, Accept: 'application/json'};
+      this.http.post(url, {}, headers).then((response: HTTPResponse) => {
+        const data = response.data as any;
+        resolve(data.access_token);
+      }).catch((error) => reject(error));
     });
   }
 
   getAllCases(token: string): Promise<number> {
     return new Promise<number>((resolve, reject) => {
-      const url = Uri.ACCESS_TOKEN;
-      const headers = new HttpHeaders();
-      headers.set('Authorization', `Bearer ${token}`);
-      this.http.get<CaseResponse[]>(url, {headers}).subscribe((response: CaseResponse[]) => {
-        const cases: number = response.reduce((total, x) => total + x.cases, 0);
-        resolve(cases);
-      }, (error) => reject(error));
+      const url = Uri.CASES;
+      const headers = {Authorization: `Bearer ${token}`, Accept: 'application/json'};
+      this.http.get(url, {}, headers).then((response: HTTPResponse) => {
+        const data = response.data as any[];
+        resolve(data.reduce((total, x) => total + x.cases, 0));
+      }).catch((error) => reject(error));
     });
   }
 
   getConfirmedCases(token: string): Promise<number> {
     return new Promise<number>((resolve, reject) => {
-      const url = Uri.ACCESS_TOKEN;
-      const headers = new HttpHeaders();
-      headers.set('Authorization', `Bearer ${token}`);
-      this.http.get<CaseResponse[]>(url, {headers}).subscribe((response: CaseResponse[]) => {
-        const data: number = response.reduce((total, x) => total + x.data, 0);
-        resolve(data);
-      }, (error) => reject(error));
+      const url = Uri.CONFIRMED_CASES;
+      const headers = {Authorization: `Bearer ${token}`, Accept: 'application/json'};
+      this.http.get(url, {}, headers).then((response: HTTPResponse) => {
+        const data = response.data as any[];
+        resolve(data.reduce((total, x) => total + x.data, 0));
+      }).catch((error) => reject(error));
     });
   }
 
   getSuspectedCases(token: string): Promise<number> {
     return new Promise<number>((resolve, reject) => {
-      const url = Uri.ACCESS_TOKEN;
-      const headers = new HttpHeaders();
-      headers.set('Authorization', `Bearer ${token}`);
-      this.http.get<CaseResponse[]>(url, {headers}).subscribe((response: CaseResponse[]) => {
-        const data: number = response.reduce((total, x) => total + x.data, 0);
-        resolve(data);
-      }, (error) => reject(error));
+      const url = Uri.SUSPECTED_CASES;
+      const headers = {Authorization: `Bearer ${token}`, Accept: 'application/json'};
+      this.http.get(url, {}, headers).then((response: HTTPResponse) => {
+        const data = response.data as any[];
+        resolve(data.reduce((total, x) => total + x.data, 0));
+      }).catch((error) => reject(error));
     });
   }
 
   getDeaths(token: string): Promise<number> {
     return new Promise<number>((resolve, reject) => {
-      const url = Uri.ACCESS_TOKEN;
-      const headers = new HttpHeaders();
-      headers.set('Authorization', `Bearer ${token}`);
-      this.http.get<CaseResponse[]>(url, {headers}).subscribe((response: CaseResponse[]) => {
-        const data: number = response.reduce((total, x) => total + x.data, 0);
-        resolve(data);
-      }, (error) => reject(error));
+      const url = Uri.DEATHS;
+      const headers = {Authorization: `Bearer ${token}`, Accept: 'application/json'};
+      this.http.get(url, {}, headers).then((response: HTTPResponse) => {
+        const data = response.data as any[];
+        resolve(data.reduce((total, x) => total + x.data, 0));
+      }).catch((error) => reject(error));
     });
   }
 
   getRecovered(token: string): Promise<number> {
     return new Promise<number>((resolve, reject) => {
-      const url = Uri.ACCESS_TOKEN;
-      const headers = new HttpHeaders();
-      headers.set('Authorization', `Bearer ${token}`);
-      this.http.get<CaseResponse[]>(url, {headers}).subscribe((response: CaseResponse[]) => {
-        const data: number = response.reduce((total, x) => total + x.data, 0);
-        resolve(data);
-      }, (error) => reject(error));
+      const url = Uri.RECOVERED;
+      const headers = {Authorization: `Bearer ${token}`, Accept: 'application/json'};
+      this.http.get(url, {}, headers).then((response: HTTPResponse) => {
+        const data = response.data as any[];
+        resolve(data.reduce((total, x) => total + x.data, 0));
+      }).catch((error) => reject(error));
     });
   }
 }
